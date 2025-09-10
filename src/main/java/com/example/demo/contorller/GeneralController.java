9870630ad42da49f04a4bdd467d915636636a935
Kirito140 <maxence.camporurcy@gmail.com>
2025-09-10 15:02:37 +0200

    feat: interface utilisateur CRUD, contrôleurs, config exemple et intégration MySQL

--- src/main/java/com/example/demo/contorller/GeneralController.java
+++ src/main/java/com/example/demo/contorller/GeneralController.java
@@ -1,11 +1,8 @@
 package com.example.demo.contorller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GeneralController {
