66d93097d7070d81a09536d2b54206e64e487fdd
Kirito140 <maxence.camporurcy@gmail.com>
2025-09-11 19:22:36 +0200

    docker avec db et tout le tralala

--- init.sql
+++ init.sql
@@ -1,11 +1,14 @@
 CREATE DATABASE IF NOT EXISTS demo;
 USE demo;
 
-CREATE TABLE IF NOT EXISTS user (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    email VARCHAR(100)
-);
+CREATE TABLE `user` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email` (`email`),
+    KEY `idx_user_email` (`email`)
+) ENGINE = InnoDB AUTO_INCREMENT = 8 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
 
 INSERT INTO user (name, email) VALUES
 ('Alice', 'alice@example.com'),
