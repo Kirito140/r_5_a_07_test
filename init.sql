0b273db7b64c1b82d8299cc831df25b711ae3b72
Kirito140 <maxence.campourcy@gmail.com>
2025-09-14 22:10:07 +0200

    correction init.sql

--- init.sql
+++ init.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE IF NOT EXISTS demo;
 USE demo;
 
-CREATE TABLE `user` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `name` varchar(100) NOT NULL,
-    `email` varchar(100) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `email` (`email`),
-    KEY `idx_user_email` (`email`)
-) ENGINE = InnoDB AUTO_INCREMENT = 8 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY uq_email (email),
+    KEY idx_user_email (email)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO user (name, email) VALUES
+INSERT INTO users (name, email) VALUES
 ('Alice', 'alice@example.com'),
 ('Bob', 'bob@example.com');
