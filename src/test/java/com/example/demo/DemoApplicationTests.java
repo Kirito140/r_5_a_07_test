a64de77f35184934a8ceaa72909434eb08be76a0
Solacroup Corentin <181864359+MrcotinSol@users.noreply.github.com>
2025-09-16 15:27:11 +0200

    reprise de test sur connexion DB

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,10 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.example.demo.services.SQL;
 
 @SpringBootTest
 class DemoApplicationTests {
-
-    @Mock
-    private SQL sql;
 
     @Test
     void contextLoads() {
